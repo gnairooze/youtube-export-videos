d11a126e7f98c90f4d58f667035eff017dc5aefa
George Nairooze <george.nairooze@linkdev.com>
2022-09-16 19:36:40 +0200

    first commit

---
+++ src/youtueb.export.videos/youtube.export.videos.app/ExportVideos.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Channels;
+using YoutubeExplode.Common;
+using YoutubeExplode.Playlists;
+using Channel = YoutubeExplode.Channels.Channel;
+
+namespace youtube.export.videos.app
+{
+    internal class ExportVideos
+    {
+        private static YoutubeClient _Youtube = new YoutubeClient();
+
+        public static async Task ExportChannel()
+        {
+            try
+            {
+                #region Step 1
+                Console.Write("Enter YouTube Channel URL: ");
+                var url = Console.ReadLine() ?? "";
+
+                var channel = await _Youtube.Channels.GetAsync(url);
+                #endregion
+
+                await Step2(channel);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            
+        }
+
+        private static async Task Step2(Channel channel)
+        {
+            var videos = await _Youtube.Channels.GetUploadsAsync(channel.Url);
+
+            WriteMD(channel, videos);
+
+            Console.WriteLine("Done");
+        }
+
+        public static async Task ExportUser()
+        {
+            try
+            {
+                #region Step 1
+                Console.Write("Enter YouTube User URL: ");
+                var url = Console.ReadLine() ?? "";
+
+                var channel = await _Youtube.Channels.GetByUserAsync(url);
+                #endregion
+
+                await Step2(channel);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            
+        }
+
+        public static async Task ExportSlug()
+        {
+            try
+            {
+                #region Step 1
+                Console.Write("Enter YouTube Slug URL: ");
+                var url = Console.ReadLine() ?? "";
+
+                var channel = await _Youtube.Channels.GetBySlugAsync(url);
+                #endregion
+
+                await Step2(channel);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+        }
+
+        private static void WriteMD(Channel channel, IReadOnlyList<PlaylistVideo> videos)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9 -]");
+            var title = rgx.Replace(channel.Title, "");
+            var createdate = new DateTime(DateTime.Now.Ticks);
+            var createtext = createdate.ToString("yyyy-MM-dd-HH-mm-fff");
+
+            var filename = $"{title}-{createtext}.md";
+
+            System.IO.File.AppendAllText(filename, $"# [{channel.Title}]({channel.Url}){Environment.NewLine}");
+
+            System.IO.File.AppendAllText(filename, $"{videos.Count} videos exported on {createdate.ToString("yyyy-MM-dd HH:mm")}{Environment.NewLine}{Environment.NewLine}");
+
+            int counter = 1;
+
+            var counterPattern = $"D{Math.Floor(Math.Log10(videos.Count)) + 1}";
+
+            foreach (var video in videos)
+            {
+                System.IO.File.AppendAllText(filename, $"{counter.ToString(counterPattern)}. [{video.Title}]({video.Url}){Environment.NewLine}![](https://i.ytimg.com/vi/{video.Id}/mqdefault.jpg){Environment.NewLine}{video.Duration} {Environment.NewLine}{Environment.NewLine}");
+
+                counter++;
+            }
+        }
+    }
+}
---
+++ src/youtueb.export.videos/youtube.export.videos.app/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using youtube.export.videos.app;
+
+Console.Title = "Export Youtube Videos";
+
+while (true)
+{
+    var answer = AskQuestion();
+
+    Console.WriteLine();
+
+    switch (answer.Key)
+    {
+        case ConsoleKey.NumPad1:
+        case ConsoleKey.D1:
+            await ExportVideos.ExportChannel();
+            break;
+        case ConsoleKey.NumPad2:
+        case ConsoleKey.D2:
+            await ExportVideos.ExportUser();
+            break;
+        case ConsoleKey.NumPad3:
+        case ConsoleKey.D3:
+            await ExportVideos.ExportSlug();
+            break;
+        case ConsoleKey.NumPad4:
+        case ConsoleKey.D4:
+            Environment.Exit(0);
+            break;
+    }
+}
+
+static ConsoleKeyInfo AskQuestion()
+{
+    Console.WriteLine($"{Environment.NewLine}");
+    Console.WriteLine("Please select one of the following:");
+    Console.WriteLine("1. Export videos by Youtube channel url (example: https://www.youtube.com/channel/[code])");
+    Console.WriteLine("2. Export videos by Youtube user url (example: https://www.youtube.com/user/[username])");
+    Console.WriteLine("3. Export videos by Youtube slug url (example: https://www.youtube.com/c/[name])");
+    Console.WriteLine("4. Exit");
+    Console.WriteLine();
+
+    var answer = Console.ReadKey();
+    return answer;
+}
